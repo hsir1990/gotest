0ad4f5d9220ca0dd9ec9cba3e1c6f45130c32b5d
hsir1990 <hsir1990@163.com>
2022-03-03 23:32:40 +0800

    207

--- 003func/function.go
+++ 003func/function.go
@@ -11,6 +11,7 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"time"
 )
 
 //大写定义全局，整个系统有效
@@ -110,6 +111,102 @@ func main() {
 	//16将字符串左右两边的空格去掉 string.TrimSpace("  sdfad df sdf  ")
 	str8 = strings.TrimSpace("  sdfad df sdf  ")
 	fmt.Println("\"" + str8 + "\"")
+
+	//17将字符串左右两边的指定的字符去掉 
+	//strings.Trim("! hello! ","!") //["hello"] 将左右两边的 ！和“ ”去掉
+	str8 = strings.Trim("! h el! lo! "," !")  //h el! lo
+	fmt.Printf("str8 = %q \n",str8)  
+	//%q该值对应的单引号括起来的go语法字符字面值，必要时会采用安全的转义表示
+
+	//18将字符串左边指定的字符去掉：strings.TrimLeft("! hello! ","! ") //["hello"]//将左边！和 “ ”
+	//19将字符串左边指定的字符去掉：strings.TrimRight("! hello! ","! ") //["hello"]//将左边！和 “ ”
+	//20判断字符串是否以指定的字符串开头： strings.HasPrefix("ftp://192.168.10.1","ftp") //true
+
+	var b3  bool = strings.HasPrefix("ftp://192.168.10.1","ftp")
+	fmt.Printf("b3=%v \n",b3)
+
+	//21判断字符串是否以指定的字符串结束： string.HasSuffix("sdfjkl.jpg","jpg") //true
+
+	//1. 日期 time的类型==time.Time，
+	//值是==2022-03-03 22:11:13.5509589 +0800 CST m=+0.002122001
+	now := time.Now()  //time.Time
+	fmt.Printf("time的类型==%T，值是==%v \n",now,now)
+	//time的类型==time.Time，值是==2022-03-03 22:11:13.5509589 +0800 CST m=+0.002122001
+
+	//2. 通过now可以获取到年月日，时分秒
+	fmt.Printf("年=%v \n",now.Year())
+	fmt.Printf("月=%v \n",now.Month())
+	fmt.Printf("月=%v \n",int(now.Month()))
+	fmt.Printf("日=%v \n",now.Day())
+	fmt.Printf("时=%v \n",now.Hour())
+	fmt.Printf("分=%v \n",now.Minute())
+	fmt.Printf("秒=%v \n",now.Second())
+
+
+	//格式化日期时间
+	//方式1：就是使用Printf 或者 SPrintf
+	fmt.Printf("当1前年月日 %d-%d-%d %d:%d:%d \n",now.Year(),
+	now.Month(),now.Day(),now.Hour(),now.Minute(),now.Second())
+	
+	dateStr := fmt.Sprintf("当2前年月日 %d-%d-%d %d:%d:%d \n",now.Year(),
+	now.Month(),now.Day(),now.Hour(),now.Minute(),now.Second())
+	
+	fmt.Printf("dataStr = %v \n",dateStr)
+
+
+	//方式二 使用time.Format()方法完成
+	fmt.Printf(now.Format("2006-01-02 15:04:05"))
+	fmt.Println()
+	fmt.Printf(now.Format("2006-01-02"))
+	fmt.Println()
+	fmt.Printf(now.Format("15:04:05"))
+	fmt.Println()
+
+	//"2006-01-02 15:04:05" 是固定的数字
+
+	// 时间常量
+
+	// 100 * time.Millisecond
+
+	//纳秒 Nanosecond Duration = 1 
+	//微秒 Microsecond 
+	//毫秒 Millisecond 
+	//秒 Second
+	//分 Minute
+	//时 Hour 
+	i:=1
+	for{
+		i++
+		fmt.Println(i)
+		time.Sleep(time.Millisecond *10)
+		if i == 10{
+			break
+		}
+	}
+
+
+	// 7.time的Unix 和UnixNano的方法
+	fmt.Printf("unix时间戳=%v  unixnano时间戳=%v \n", now.Unix(),now.UnixNano())
+
+
+
+	//内置函数
+	//1. len用来求长度 比如 string array slice map channel
+
+	//2.new 用来分配内存，主要用来分配 值类型 ，比如 int,float32,struct...返回的是指针 
+	num1 := 100
+	fmt.Printf("num1的类型%T，num1的值=%v，num1的地址=%v \n",num1,num1,&num1)
+	num2 := new(int) //  可以类比成 *int  等于是new出一个地址，然后，这个地址再指向一个值，*num2 = 100，
+	//num2的类型%T =》 *int
+	//num2的值 = 地址 0xc04204c098 (这个地址是系统分配的)
+	//num2的地址%v = 地址 0xc04206a020 （这个地址是系统分配的）
+	//num2指向的值 = 100
+	*num2 = 100
+	fmt.Printf("num2的类型 %T，num2的值 =%v, num2的地址%v \n,num2这个指针，指向的值=%v",
+	num2,num2,&num2,*num2)
+
+
+	//3.make 用来分配内存，主要用来分配引用类型，比如 channel,map,slice.
 }
 
 //编写一个makeSuffix(suffix string) 可以接收一个文件后缀名（比如 .jpg），
